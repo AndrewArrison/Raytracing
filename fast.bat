ea3ee84cf68709e548c96a08200ad9d0b20633c3
AndrewArrison <alwinwithjesus@gmail.com>
2025-06-04 12:38:32 +0530

    added SDL for showing image upon completion, needs refactoring

---
+++ fast.bat
@@ -0,0 +1,1 @@
+g++ -o ray.exe main.cpp -IE:/main/plsc/mingw32/include -LE:/main/plsc/mingw32/lib -lmingw32 -lSDL3 E:/main/plsc/mingw32/lib/libSDL3.a
--- main.cpp
+++ main.cpp
@@ -1,37 +1,94 @@
 // ================================================
 // File: main.cpp
 // Created on: 2025-06-03 21:17:33
-// Last modified: 2025-06-03 22:18:56
+// Last modified: 2025-06-04 12:33:58
 // Created by: Alwin R Ajeesh
 // ================================================
-
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL.h>
 #include <iostream>
 
 int image_width = 2560;
 int image_height = 2560;
 
-void render()
+int window_width = 640;
+int window_height = 480;
+
+uint32_t pack_color(double r, double g, double b)
 {
-	std::cout << "P3\n" << image_width << " " << image_height << "\n" << "255\n";
+	int ir = static_cast<int>(255.999 * r);
+	int ig = static_cast<int>(255.999 * g);
+	int ib = static_cast<int>(255.999 * b);
 
-	for (int j = 0; j < image_height; j++) {
-		for (int i = 0; i < image_width; i++) {
-			double r = (double)(i) / (double)(image_width);
-			double g = (double)(j) / (double)(image_height);
-			double b = 0.2;
-
-			int ir = int(255.999*r);
-			int ig = int(255.999*g);
-			int ib = int(255.999*b);
-
-			std::cout << ir << " " << ig << " " << ib << "\n";
-		}
-	}	
+	return (ir << 16) | (ig << 8) | ib; // XRGB8888
 }
 
 int main()
 {
-	render();
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cerr << "SDL init failed\n" << SDL_GetError() << "\n";
+		return 1;
+	}
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	SDL_Texture* texture = nullptr;
+
+	if (!SDL_CreateWindowAndRenderer("ray", window_width, window_height, 0, &window, &renderer)) {
+		std::cerr << "window or renderer fail.\n" << SDL_GetError() << "\n";
+		SDL_Quit();
+		return 1;
+	}
+
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_XRGB8888, SDL_TEXTUREACCESS_STREAMING, image_width, image_height);
+	
+	//pixel buffer
+	uint32_t* pixels = new uint32_t[image_width * image_height];
+	//Fill pixel buffer with image data
+	for (int j = 0; j < image_height; j++) {
+		std::clog << "\rScanlines remaining : " << (image_height - j) << " " << std::flush;
+		for (int i = 0; i < image_width; i++) {
+			double r = double(i) / image_width;
+			double g = double(j) / image_height;
+			double b = 0.2;
+			
+			pixels[j * image_width + i] = pack_color(r, g, b);
+		}
+	}
+	
+	std::clog << "\r Done.                        \n";
+
+	SDL_UpdateTexture(texture, nullptr, pixels, image_width * sizeof(uint32_t));
+
+	SDL_RenderClear(renderer);
+	SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+	SDL_RenderPresent(renderer);
+	
+	bool running = true;
+	while(running)
+	{
+		SDL_Event event;
+		while(SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_EVENT_QUIT:
+					running = false;
+					break;
+
+				default:
+					break;
+			}
+		}
+		// SDL_RenderClear(renderer);
+		// SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+		// SDL_RenderPresent(renderer);
+		SDL_Delay(16);
+	}
+
+	delete[] pixels;
+	SDL_DestroyTexture(texture);
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
 	return 0;
 }
 
