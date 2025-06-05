2f5fa5bb8320e408b163dc225c3bc54131b8d0a4
AndrewArrison <alwinwithjesus@gmail.com>
2025-06-05 18:09:45 +0530

    dir changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 // ================================================
 // File: main.cpp
 // Created on: 2025-06-03 21:17:33
-// Last modified: 2025-06-05 11:47:02
+// Last modified: 2025-06-05 15:13:41
 // Created by: Alwin R Ajeesh
 // ================================================
 #include <GLM/common.hpp>
 #include <GLM/ext/vector_double3.hpp>
 #include <GLM/geometric.hpp>
 #include <cmath>
+#include <vector>
 #define SDL_MAIN_HANDLED
 #include <SDL3/SDL.h>
 #include <iostream>
 #include "ray.hpp"
+#include "Timer.hpp"
+#include "sphere.hpp"
 
 
 int aspect_ratio = 16.0/9.0;
-int image_width = 400;
+int image_width = 500;
 int image_height = int(image_width / aspect_ratio);
 
 int viewport_height = 2.0;
@@ -24,19 +27,6 @@ int viewport_width = viewport_height * (double(image_width)/image_height);
 int window_width = 500;
 int window_height = 500;
 
-double hit_sphere(const glm::dvec3& center, double radius, const ray& r)
-{
-	glm::dvec3 oc = center - r.getOrigin();
-	double a = glm::dot((r.getDirection()), r.getDirection());
-	double h = glm::dot(r.getDirection(), oc);
-	double c = glm::dot(oc, oc) - radius*radius;
-	double discriminant = h*h-a*c;
-	if (discriminant < 0) {
-		return -1;
-	} else {
-		return (h - std::sqrt(discriminant)) / a;
-	}
-}
 
 uint32_t pack_color(glm::dvec3& color)
 {
@@ -47,15 +37,26 @@ uint32_t pack_color(glm::dvec3& color)
 	return (ir << 16) | (ig << 8) | ib; // XRGB8888
 }
 
-glm::dvec3 ray_color(const ray& r)
+glm::dvec3 ray_color(const ray& r, const std::vector<Sphere>& spheres)
 {
-	double t = hit_sphere(glm::dvec3(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-		glm::dvec3 Normal = glm::normalize(r.at(t) - glm::dvec3(0, 0, -1));
-		glm::dvec3 l = glm::normalize(glm::dvec3(1, 1, 0.4) - r.at(t));
-		double f = glm::max(0.0, glm::dot(Normal, l));
-		// return 0.5*(Normal + glm::dvec3(1.0));
-		return glm::dvec3(1,0,1)*f;
+	// double t = hit_sphere(glm::dvec3(0, 0, -1), 0.5, r);
+	for (Sphere sphere : spheres) {
+		double t = sphere.intersect(r);
+		if (t > 0.0) {
+			glm::dvec3 hit_point = r.at(t);
+			glm::dvec3 Normal = glm::normalize(hit_point - glm::dvec3(0, 0, -1));
+			glm::dvec3  light_pos = glm::dvec3(1, 1, 0.4);
+			//vector from surface to light
+			glm::dvec3 light_dir = glm::normalize(light_pos - hit_point);
+			//normal dot light = cos(angle)
+			double diffuse_intensity = glm::max(0.0, glm::dot(Normal, light_dir));
+			//base colour of sphere
+			glm::dvec3 albedo(0.8, 0.3, 0.1);
+			//light color
+			glm::dvec3 light_color(1.0, 0.5, 0.5);
+			return 0.5*(Normal + glm::dvec3(1.0));
+			// return albedo * light_color * diffuse_intensity;
+		}
 	}
 	glm::dvec3 unit_direction = r.getDirection();
 	double blend_factor = 0.5*(unit_direction.y + 1.0);
@@ -63,26 +64,27 @@ glm::dvec3 ray_color(const ray& r)
 	return color;
 }
 
-void render(SDL_Renderer* renderer, SDL_Texture* texture, glm::dvec3& camera_center, glm::dvec3& pixel00_loc, glm::dvec3& pixel_delta_w, glm::dvec3& pixel_delta_h, SDL_FRect& rect)
+void render(SDL_Renderer* renderer, SDL_Texture* texture, glm::dvec3& camera_center, glm::dvec3& pixel00_loc, glm::dvec3& pixel_delta_w, glm::dvec3& pixel_delta_h, SDL_FRect& rect, std::vector<Sphere> spheres)
 {
 	//pixel buffer
 	uint32_t* pixels = new uint32_t[image_width * image_height];
 	//Fill pixel buffer with image data
+	Timer time;
 	for (int j = 0; j < image_height; j++) {
 		for (int i = 0; i < image_width; i++) {
 			glm::dvec3 pixel_center = pixel00_loc + ( double(i) * pixel_delta_w) + ( double(j) * pixel_delta_h);
 			glm::dvec3 ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
-			glm::dvec3 pixel_color = ray_color(r);
+			glm::dvec3 pixel_color = ray_color(r, spheres);
 			pixels[j * image_width + i] = pack_color(pixel_color);
 		}
-		SDL_UpdateTexture(texture, nullptr, pixels, image_width * sizeof(uint32_t));
-		SDL_RenderTexture(renderer, texture, nullptr, &rect);
-		SDL_RenderPresent(renderer);
-		std::clog << "\rScanlines remaining : " << (image_height - j) << " " << std::flush;
+		// std::clog << "\rScanlines remaining : " << (image_height - j) << " " << std::flush;
 	}
+	SDL_UpdateTexture(texture, nullptr, pixels, image_width * sizeof(uint32_t));
+	SDL_RenderTexture(renderer, texture, nullptr, &rect);
+	SDL_RenderPresent(renderer);
 	delete[] pixels;
-	std::clog << "\rDone.                        \n";
+	// std::clog << "\rDone.                        \n";
 }
 
 	
@@ -115,7 +117,11 @@ int main()
 	glm::dvec3 pixel00_loc = viewport_ul + 0.5*(pixel_delta_w + pixel_delta_h);
 	SDL_FRect rect = {0, 0, 400, 400};
 
-	render(renderer, texture, camera_center, pixel00_loc, pixel_delta_w, pixel_delta_h, rect);
+	std::vector<Sphere> Spheres;
+	Spheres.push_back(Sphere(glm::dvec3(0.0, 0.0, -1.0), 0.5));
+	Spheres.push_back(Sphere(glm::dvec3(0.7, 0.5, -1.5), 0.5));
+
+	render(renderer, texture, camera_center, pixel00_loc, pixel_delta_w, pixel_delta_h, rect, Spheres);
 
 	bool running = true;
 	while(running)
@@ -129,7 +135,7 @@ int main()
 				case SDL_EVENT_KEY_DOWN:
 					switch (event.key.scancode) {
 						case SDL_SCANCODE_R:
-							render(renderer, texture, camera_center, pixel00_loc, pixel_delta_w, pixel_delta_h, rect);
+							render(renderer, texture, camera_center, pixel00_loc, pixel_delta_w, pixel_delta_h, rect, Spheres);
 							break;
 						default:
 							break;
---
+++ src/sphere.hpp
@@ -0,0 +1,40 @@
+// ================================================
+// File: sphere.hpp
+// Created on: 2025-06-05 14:36:58
+// Last modified: 2025-06-05 14:47:46
+// Created by: Alwin R Ajeesh
+// ================================================
+
+#include "ray.hpp"
+#include <GLM/ext/vector_double3.hpp>
+
+class Sphere
+{
+
+public:
+	Sphere() { }
+	Sphere(const glm::dvec3& center, const double radius) : m_Center(center), m_Radius(radius) { }
+
+	double intersect(const ray& r) const;
+
+	
+private:
+	glm::dvec3 m_Center;
+	double m_Radius;
+
+};
+
+double Sphere::intersect(const ray& r) const
+{	
+	glm::dvec3 oc = m_Center - r.getOrigin();
+	double a = glm::dot((r.getDirection()), r.getDirection());
+	double h = glm::dot(r.getDirection(), oc);
+	double c = glm::dot(oc, oc) - m_Radius*m_Radius;
+	double discriminant = h*h-a*c;
+	if (discriminant < 0) {
+		return -1;
+	} else {
+		//Closest t (hitpoint from camera)
+		return (h - std::sqrt(discriminant)) / a;
+	}
+}
